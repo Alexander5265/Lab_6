2f5e1ea5fa4362b47bae5101808e88260de0c592
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-04-14 21:22:29 +0500

    hi

---
+++ 6_1/6_1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Тест конструкторов - ");
+
+        Triangle t1 = new Triangle();
+        Console.WriteLine("По умолчанию: " + t1);
+
+        Triangle t2 = new Triangle(3, 4, 5);
+        Console.WriteLine("С параметрами: " + t2);
+
+        Triangle t3 = new Triangle(t2);
+        Console.WriteLine("Копия: " + t3);
+
+        Console.WriteLine("\nПроверка метода Exists - ");
+        Console.WriteLine($"{t2} -> {t2.Exists()}");
+
+        Console.WriteLine("\nВвод пользователя - ");
+
+        try
+        {
+            Console.Write("Введите сторону A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите сторону B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите сторону C: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Triangle userTriangle1 = new Triangle(a, b, c);
+
+            Console.WriteLine("Ваш треугольник: " + userTriangle1);
+            Console.WriteLine("Существует: " + userTriangle1.Exists());
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка: введено не число!");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        
+        // Для 3 лабы
+        Triangle tr1 = new Triangle(3, 4, 5);
+        Triangle tr2 = new Triangle(6, 8, 10);
+
+        Console.WriteLine("\nТреугольник 1: " + tr1);
+        Console.WriteLine("Площадь: " + tr1.GetArea());
+
+        Console.WriteLine("\nТреугольник 2: " + tr2);
+        Console.WriteLine("Площадь: " + tr2.GetArea());
+        
+        double perimeter = (double)tr1;
+        Console.WriteLine($"\nПериметр t1: {perimeter}");
+        
+        if (tr1)
+            Console.WriteLine("t1 существует");
+        else
+            Console.WriteLine("t1 не существует");
+        
+        Console.WriteLine("\nСравнение площадей:");
+        Console.WriteLine($"t1 > t2: {tr1 > tr2}");
+        Console.WriteLine($"t1 < t2: {tr1 < tr2}");
+    }
+}
---
+++ 6_1/6_1/Testing.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+class Triangle
+{
+    private double a;
+    private double b;
+    private double c;
+
+    // Свойства с проверкой
+    public double A
+    {
+        get => a;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Сторона должна быть больше 0");
+            a = value;
+        }
+    }
+
+    public double B
+    {
+        get => b;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Сторона должна быть больше 0");
+            b = value;
+        }
+    }
+
+    public double C
+    {
+        get => c;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Сторона должна быть больше 0");
+            c = value;
+        }
+    }
+
+    // Конструктор по умолчанию
+    public Triangle()
+    {
+        A = 1;
+        B = 1;
+        C = 1;
+    }
+
+    // Конструктор с параметрами
+    public Triangle(double a, double b, double c)
+    {
+        A = a;
+        B = b;
+        C = c;
+    }
+
+    // Конструктор копирования
+    public Triangle(Triangle other)
+    {
+        if (other == null)
+            throw new ArgumentNullException(nameof(other));
+
+        A = other.A;
+        B = other.B;
+        C = other.C;
+    }
+
+    // Метод проверки существования
+    public bool Exists()
+    {
+        return A + B > C &&
+               A + C > B &&
+               B + C > A;
+    }
+    
+    // УНАРНАЯ операция (площадь по формуле Герона)
+    public double GetArea()
+    {
+        double p = (A + B + C) / 2; // полупериметр
+        return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+    }
+
+    // Приведение к double (периметр)
+    public static explicit operator double(Triangle t)
+    {
+        return t.A + t.B + t.C;
+    }
+
+    // Приведение к bool (существует ли)
+    public static implicit operator bool(Triangle t)
+    {
+        return t.Exists();
+    }
+
+    // Бинарные операции (сравнение площадей)
+    public static bool operator >(Triangle t1, Triangle t2)
+    {
+        return t1.GetArea() > t2.GetArea();
+    }
+
+    public static bool operator <(Triangle t1, Triangle t2)
+    {
+        return t1.GetArea() < t2.GetArea();
+    }
+    
+    public override string ToString()
+    {
+        return $"A={A}, B={B}, C={C}";
+    }
+}
