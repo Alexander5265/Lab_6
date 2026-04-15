91a8a8a888e97fb93be32a9f69757c39d3dfe160
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-04-15 09:18:37 +0500

    Update Testing.cs

--- 6_1/6_1/Testing.cs
+++ 6_1/6_1/Testing.cs
@@ -1,12 +1,11 @@
-﻿using System;
+using System;
 
 class Triangle
 {
     private double a;
     private double b;
     private double c;
-
-    // Свойства с проверкой
+    
     public double A
     {
         get => a;
@@ -39,24 +38,21 @@ class Triangle
             c = value;
         }
     }
-
-    // Конструктор по умолчанию
+    
     public Triangle()
     {
         A = 1;
         B = 1;
         C = 1;
     }
-
-    // Конструктор с параметрами
+    
     public Triangle(double a, double b, double c)
     {
         A = a;
         B = b;
         C = c;
     }
-
-    // Конструктор копирования
+    
     public Triangle(Triangle other)
     {
         if (other == null)
@@ -66,8 +62,7 @@ class Triangle
         B = other.B;
         C = other.C;
     }
-
-    // Метод проверки существования
+    
     public bool Exists()
     {
         return A + B > C &&
@@ -75,26 +70,22 @@ class Triangle
                B + C > A;
     }
     
-    // УНАРНАЯ операция (площадь по формуле Герона)
     public double GetArea()
     {
-        double p = (A + B + C) / 2; // полупериметр
+        double p = (A + B + C) / 2;
         return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
     }
-
-    // Приведение к double (периметр)
+    
     public static explicit operator double(Triangle t)
     {
         return t.A + t.B + t.C;
     }
-
-    // Приведение к bool (существует ли)
+    
     public static implicit operator bool(Triangle t)
     {
         return t.Exists();
     }
-
-    // Бинарные операции (сравнение площадей)
+    
     public static bool operator >(Triangle t1, Triangle t2)
     {
         return t1.GetArea() > t2.GetArea();
