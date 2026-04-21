35b409cab2097daa443595faf47c55f79fe460c2
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-04-21 21:25:47 +0500

    Update Testing.cs

--- 6_1/6_1/Testing.cs
+++ 6_1/6_1/Testing.cs
@@ -1,103 +1,53 @@
-using System;
 
-class Triangle
+public class Triangle
 {
-    private double a;
-    private double b;
-    private double c;
-    
-    public double A
-    {
-        get => a;
-        set
-        {
-            if (value <= 0)
-                throw new ArgumentException("Сторона должна быть больше 0");
-            a = value;
-        }
-    }
+    private double sideA;
+    private double sideB;
+    private double sideC;
 
-    public double B
-    {
-        get => b;
-        set
-        {
-            if (value <= 0)
-                throw new ArgumentException("Сторона должна быть больше 0");
-            b = value;
-        }
-    }
-
-    public double C
-    {
-        get => c;
-        set
-        {
-            if (value <= 0)
-                throw new ArgumentException("Сторона должна быть больше 0");
-            c = value;
-        }
-    }
-    
     public Triangle()
     {
-        A = 1;
-        B = 1;
-        C = 1;
+        Console.Write("Введите сторону A: ");
+        sideA = double.Parse(Console.ReadLine());
+        Console.Write("Введите сторону B: ");
+        sideB = double.Parse(Console.ReadLine());
+        Console.Write("Введите сторону C: ");
+        sideC = double.Parse(Console.ReadLine());
     }
     
-    public Triangle(double a, double b, double c)
+    public Triangle(double sideA, double sideB, double sideC)
     {
-        A = a;
-        B = b;
-        C = c;
-    }
-    
-    public Triangle(Triangle other)
-    {
-        if (other == null)
-            throw new ArgumentNullException(nameof(other));
-
-        A = other.A;
-        B = other.B;
-        C = other.C;
-    }
-    
-    public bool Exists()
-    {
-        return A + B > C &&
-               A + C > B &&
-               B + C > A;
-    }
-    
-    public double GetArea()
-    {
-        double p = (A + B + C) / 2;
-        return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
-    }
-    
-    public static explicit operator double(Triangle t)
-    {
-        return t.A + t.B + t.C;
-    }
-    
-    public static implicit operator bool(Triangle t)
-    {
-        return t.Exists();
-    }
-    
-    public static bool operator >(Triangle t1, Triangle t2)
-    {
-        return t1.GetArea() > t2.GetArea();
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
     }
 
-    public static bool operator <(Triangle t1, Triangle t2)
+    public bool Existance()
     {
-        return t1.GetArea() < t2.GetArea();
+        return sideA + sideB > sideC &&
+               sideA + sideC > sideB &&
+               sideB + sideC > sideC;
     }
     
-    public override string ToString()
+    public double Area()
     {
-        return $"A={A}, B={B}, C={C}";
+        double p = (sideA + sideB + sideC) / 2;
+        return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+    }
+    
+    public double Perimeter()
+    {
+        return sideA + sideB + sideC;
+    }
+    
+    // Бинарные операции (сравнение площадей)
+    public static bool operator >(Triangle triangle1, Triangle triangle2)
+    {
+        return triangle1.Area() > triangle2.Area();
+    }
+
+    public static bool operator <(Triangle triangle1, Triangle triangle2)
+    {
+        return triangle1.Area() < triangle2.Area();
     }
 }
