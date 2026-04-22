d5ff3d33243cefa4c3f5c25b2bc4e185d8b09714
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-04-22 09:15:10 +0500

    Update Program.cs

--- 6_1/6_1/Program.cs
+++ 6_1/6_1/Program.cs
@@ -4,21 +4,20 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        double sideA, sideB, sideC;
-        Console.Write("Введите стороны треугольника #1:\n ");
+        Console.Write("Введите стороны треугольника #1: ");
         Triangle triangle1 = new Triangle();
-        Console.Write("\nВведите стороны треугольника #2:\n ");
+        Console.Write("\nВведите стороны треугольника #2: ");
         Triangle triangle2 = new Triangle();
         
-        Console.WriteLine("Ваш треугольник: " + triangle1);
+        Console.WriteLine("\nВаш треугольник #1: " + triangle1);
         Console.WriteLine("Существует: " + triangle1.Existance());
-        Console.WriteLine("\nВаш треугольник: " + triangle2);
+        Console.WriteLine("Ваш треугольник #2: " + triangle2);
         Console.WriteLine("Существует: " + triangle2.Existance());
         
-        Console.WriteLine("Площадь треугольника #1 = " + triangle1.Area());
+        Console.WriteLine("\nПлощадь треугольника #1 = " + triangle1.Area());
         Console.WriteLine("Площадь треугольника #2 = " + triangle1.Area());
         
-        Console.WriteLine("S треугольника #1 больше S треугольника #2: ");
+        Console.WriteLine("\nS треугольника #1 больше S треугольника #2: ");
         Console.WriteLine($"triangle1 > triangle2 {triangle1 > triangle2}");
         Console.WriteLine("S треугольника #2 больше S треугольника #1: ");
         Console.WriteLine($"triangle1 < triangle2 {triangle1 < triangle2}");
